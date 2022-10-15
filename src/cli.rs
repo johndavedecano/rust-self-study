dc94b60eeddabb9a736f2252204f1281290da1eb
John Dave Decano <johndavedecano@gmail.com>
2022-10-15 18:19:40 +0200

    finished up cli

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,16 @@ use std::env;
 pub fn run() {
     let args: Vec<String> = env::args().collect();
     let command = args[1].clone();
+    let name = "Brad";
+    let status = "100%";
 
-    println!("Command: {:?}", command);
+    // println!("Command: {:?}", command);
+
+    if command == "hello" {
+        println!("{} {}, how are you?",command, name);
+    } else if command == "status" {
+        println!("the status is {}", status);
+    } else {
+        println!("sorry but command was not found.");
+    }
 }
