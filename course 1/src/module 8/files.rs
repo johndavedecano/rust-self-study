196d41ff21ec2ccc208a6d75e11e1d0d33145301
John Dave Decano <johndavedecano@gmail.com>
2024-05-02 20:08:21 +0200

    error handling

---
+++ course 1/src/module 8/files.rs
@@ -0,0 +1,28 @@
+use std::{
+    fs::{remove_file, File, OpenOptions},
+    io::{Read, Write},
+};
+
+fn main() {
+    let mut file = File::create("src/example.txt").expect("create failed");
+    file.write_all("hello world".as_bytes())
+        .expect("write failed");
+
+    // let mut file = OpenOptions::new()
+    //     .append(true)
+    //     .open("src/example.txt")
+    //     .expect("cannot open file");
+
+    // file.write_all("\nAdding contents to the file".as_bytes())
+    //     .expect("unable to write to the file");
+
+    let mut file = std::fs::File::open("src/example.txt").unwrap();
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    println!("{}", contents);
+
+    remove_file("src/example.txt").expect("delete file");
+}
